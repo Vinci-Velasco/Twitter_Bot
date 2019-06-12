f8673bef997de6fc170deb6ec68d9ab23da4644a
Vinci Velasco <vincivelasco@gmail.com>
2019-06-12 13:09:55 -0700

    Initial Commit

---
+++ dogBot.py
@@ -0,0 +1,9 @@
+import config, tweepy
+
+
+consumer_key = config.consumer_key
+consumer_token = config.consumer_token
+access_token = config.access_token
+access_token_secret = config.access_token_secret
+
+
