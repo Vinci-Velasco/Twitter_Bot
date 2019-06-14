451ec28d9e16f484a0854f7308b5ffb1d613f235
Vinci Velasco <vincivelasco@gmail.com>
2019-06-13 23:30:33 -0700

    Added to .gitignore and slightly changed dogBot.py
    - Added /vscode to .gitignore
    - Removed trailing white space from dogBot.py

--- dogBot.py
+++ dogBot.py
@@ -1,4 +1,8 @@
-import config, tweepy, requests, os, time
+import config
+import os
+import time
+import requests
+import tweepy
 
 # Credentials
 consumer_key = config.consumer_key
@@ -15,23 +19,21 @@ os.chdir("./images")
 
 # Bot Loop
 while True:
-    
     # Download dog photo
     # r is an api request
     r = requests.get("https://dog.ceo/api/breed/retriever-golden/images/random")
     r.raise_for_status()
     dog_photo = r.json()["message"]
-    dog_res = requests.get(dog_photo) # request to image spefically
+    dog_res = requests.get(dog_photo)  # request to image spefically
     dog_res.raise_for_status()
 
-    # Save dog photo to images folder 
+    # Save dog photo to images folder
     dogImage = open("Dog_Photo.jpg", "wb")
     for chunk in dog_res.iter_content(100000):
         dogImage.write(chunk)
     dogImage.close()
 
     # Tweet photo
-    api.update_with_media("Dog_Photo.jpg")
-    
+    api.update_with_media("Dog_Photo.jpg")  
     # Tweet after every X time interval
     time.sleep(3600)
