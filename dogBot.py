3e614466dec24fcf07c7a48453d1ebb279d63629
Vinci Velasco <vincivelasco@gmail.com>
2019-06-18 22:07:54 -0700

    Added reply loop functionalality to dogBot.py, and other small additions
    - Defined new function reply_loop() --> every 2 hours, replies to 3 people who said "I want a dog"
      with a dog photo. Utilizes save_image() function to get image
    - Turned function into a process
    - Added a shebang line

--- dogBot.py
+++ dogBot.py
@@ -1,10 +1,10 @@
+#!/usr/bin/env python3
 import config
 import os
 import time
 import requests
 import tweepy
 from multiprocessing import Process 
-print(os.getcwd)
 
 # Credentials
 consumer_key = config.consumer_key
@@ -17,14 +17,10 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_token)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-os.chdir("./images")
-
-
 def save_image():
     # Download dog photo
     # r is an api request
-    r = requests.get
-    ("https://dog.ceo/api/breed/retriever-golden/images/random")
+    r = requests.get("https://dog.ceo/api/breed/retriever/golden/images/random")
     r.raise_for_status()
     dog_photo = r.json()["message"]
     dog_res = requests.get(dog_photo)  # request to image spefically
@@ -41,5 +37,15 @@ def tweet_loop():
         # Tweet after every hour
         time.sleep(3600)
 
+def reply_loop():
+    while True:
+        tweets = tweepy.Cursor(api.search, q="I want a dog", result_type="recent", lang="en").items(3)
+        for tweet in tweets:
+            save_image()
+            api.update_with_media("Dog_Photo.jpg", 
+            "Here's a dog photo for you! @" + tweet.user.name, tweet.id)
+        time.sleep(7200) 
+
 if __name__ == '__main__':
     Process(target=tweet_loop).start()
+    Process(target=reply_loop).start()
